9bcfcaef191e25c003500e1dd0975647a4e71eaf
Didac127 <101257249+Didac127@users.noreply.github.com>
2022-05-19 09:00:43 +0200

    Bola de foc nova sense animation player

--- Escenes/Bola_Foc.gd
+++ Escenes/Bola_Foc.gd
@@ -1,18 +1,24 @@
 extends Area2D
 
 var velocitat = 200
-
+var timer_etapes=0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play("Bola de foc animació bona")
-
-
+	var timer_etapes=0
+	scale=Vector2(0.001,0.001)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.x += velocitat * delta
+	if timer_etapes==0:
+		scale += Vector2(0.3,0.3)*delta
+	if timer_etapes==1:
+		position.x += velocitat * delta
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	if anim_name == 'Bola de foc animació bona':
+	if timer_etapes>1:
 		queue_free()
+
+
+func _on_Timer_timeout():
+	timer_etapes=timer_etapes+1
