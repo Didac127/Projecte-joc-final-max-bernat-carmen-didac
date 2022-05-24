60907481db1ad1befa83c3b2873721350d888874
Didac127 <didacmirthau@gmail.com>
2022-05-24 23:23:44 +0200

    escenes fetes
    falta boss i events

--- Escenes/NPC/Personatges guerrers/Warrior 1/Guerrer1.gd
+++ Escenes/NPC/Personatges guerrers/Warrior 1/Guerrer1.gd
@@ -10,13 +10,19 @@ var direction = 1
 var atac=false
 var atac_dreta=false
 var atac_esquerre=false
-
+var mantenir_atac=false
+var health=100
+var direccio_personatge=1
+var enemic=false
+var nom_enemic=""
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	if atac==true:
+	if health <=0:
+		$AnimatedSprite.play("Morir")
+	elif atac==true:
 		if atac_dreta==true:
 			$AnimatedSprite.play("Atac1")
 			$AnimatedSprite.flip_h = false
@@ -38,8 +44,13 @@ func _physics_process(delta):
 		
 		
 		velocity= move_and_slide(velocity,FLOOR)
-	
-	
+		
+		
+	if enemic==true:
+		if global_position>nom_enemic.global_position:
+			direction=-1
+		else:
+			direction=1
 	
 	
 	if is_on_wall():
@@ -51,17 +62,17 @@ func _physics_process(delta):
 		$RayCast2D.position.x *= -1
 		
 	
-		
+	
 
 	
-	
-	
+
 
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Persoantge principal") and atac==false:
 		atac=true
 		atac_dreta=true
+		mantenir_atac=true
 		
 
 
@@ -69,7 +80,34 @@ func _on_Area2D3_body_entered(body):
 	if body.is_in_group("Persoantge principal") and atac==false:
 		atac=true
 		atac_esquerre=true
+		mantenir_atac=true
 
 
 func _on_AnimatedSprite_animation_finished():
-	
+	if $AnimatedSprite.animation=="Atac1" and mantenir_atac==false:
+		atac=false
+		atac_dreta=false
+		atac_esquerre=false
+	if $AnimatedSprite.animation=="Morir":
+		queue_free()
+
+func _on_Area2D_body_exited(body):
+	if body.is_in_group("Persoantge principal"):
+		mantenir_atac=false
+
+
+func _on_Area2D3_body_exited(body):
+	if body.is_in_group("Persoantge principal"):
+		mantenir_atac=false
+
+func _on_Area2D4_area_entered(area):
+	if area.is_in_group("bola de foc"):
+		health=health-10
+
+
+
+func _on_Area2D2_body_entered(body):
+	if body.is_in_group("Persoantge principal"):
+		enemic=true
+		nom_enemic=body
+		
--- Escenes/Personatges principals/Personatge1/Personatgeprincipal1 bo.gd
+++ Escenes/Personatges principals/Personatge1/Personatgeprincipal1 bo.gd
@@ -8,6 +8,12 @@ var velocitat_salt=-500
 var salts=1
 var paralitzat=false
 var animacio_atacar=false
+var gravetat_guardia=false
+var vector_gravetat=Vector2(0,0)
+var posiscio_guardia=Vector2(0,0)
+var nom_guardia=""
+var cop_guardia=false
+var vida=100
 
 
 func _physics_process(delta):
@@ -20,6 +26,12 @@ func _physics_process(delta):
 			velocitat+=Vector2.RIGHT*velocitat_base
 		if Input.is_action_pressed("mou esquerre"):
 			velocitat+=Vector2.LEFT*velocitat_base
+		if gravetat_guardia==true:
+			posiscio_guardia=nom_guardia.global_position
+			vector_gravetat=posiscio_guardia-global_position
+			vector_gravetat=vector_gravetat.normalized()
+			vector_gravetat.y=0
+			velocitat+=vector_gravetat*180
 		if is_on_floor():
 			salts = 1
 		if Input.is_action_just_pressed("salta") and salts>0:
@@ -65,3 +77,25 @@ func crea_bola_foc():
 func _on_AnimatedSprite_animation_finished():
 	if $AnimatedSprite.animation=="atacar":
 		animacio_atacar=false 
+
+
+func _on_Area2D_area_entered(area):
+	if area.is_in_group("area de gravetat"):
+		gravetat_guardia=true
+		nom_guardia=area.get_parent()
+	if area.is_in_group("cop guardia"):
+		cop_guardia=true
+
+func _on_Area2D_area_exited(area):
+	if area.is_in_group("area de gravetat"):
+		gravetat_guardia=false
+	if area.is_in_group("cop guardia"):
+		cop_guardia=false
+
+
+func _on_Timer2_timeout():
+	if cop_guardia==true:
+		vida=vida-10
+		$TextureProgress.value-=10
+		if vida<=0:
+			get_tree().change_scene("res://Escenes/Menu/Menu_inicial.tscn")
